f9c7e3f711d60b393f56a2db1dcc5026d0809c46
SeungWon Seo <ttd8591@gmail.com>
2025-01-23 10:02:46 +0900

    temp

--- vp_model/temp.py
+++ vp_model/temp.py
@@ -19,7 +19,7 @@ messages = [
 ] * 3
 input_texts = processor.apply_chat_template(messages, add_generation_prompt=True)
 
-dataset_folder = "/data/ssw03270/github/LLM4Layout/vp_model/indoor_preprocessing/outputs/layouts/real_images"
+dataset_folder = "./indoor_preprocessing/outputs/layouts/real_images"
 image_paths = glob.glob(os.path.join(dataset_folder, ".png"))[:3]
 
 image_list = []
