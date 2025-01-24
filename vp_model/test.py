4ef13ad9979a63ba4fce91c762c5fc0309bb7940
SeungWon Seo <ttd8591@gmail.com>
2025-01-24 09:32:30 +0900

    test

--- vp_model/test.py
+++ vp_model/test.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # 검증 단계
     vp_model.eval()
     with torch.no_grad():
-        for idx, real_images, target_images in enumerate(test_progress_bar):
+        for idx, (real_images, target_images) in enumerate(test_progress_bar):
             real_inputs, target_inputs = accelerator.unwrap_model(vlm_model).get_inputs(real_images, target_images, device)
             target_inputs["pixel_values"] = vp_model(target_inputs["pixel_values"])
             real_texts, target_texts, = vlm_model.generate(real_inputs, target_inputs)
