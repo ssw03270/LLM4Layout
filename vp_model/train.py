3dbc9d085b030a56be5997548decc7463fd4261b
SeungWon Seo <ttd8591@gmail.com>
2025-02-05 11:57:43 +0900

    save prompt

--- vp_model/train.py
+++ vp_model/train.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
             optimizer.zero_grad()
 
             # 순전파
-            real_inputs, target_inputs = accelerator.unwrap_model(vlm_model).get_inputs(real_images, target_images, text_descriptions, device)
+            real_inputs, target_inputs, _ = accelerator.unwrap_model(vlm_model).get_inputs(real_images, target_images, text_descriptions, device)
             target_inputs["pixel_values"] = vp_model(target_inputs["pixel_values"])
             loss = vlm_model(real_inputs, target_inputs)
 
@@ -88,7 +88,7 @@ if __name__ == "__main__":
         epoch_val_loss = 0.0
         with torch.no_grad():
             for real_images, target_images, text_descriptions in val_progress_bar:
-                real_inputs, target_inputs = accelerator.unwrap_model(vlm_model).get_inputs(real_images, target_images, text_descriptions, device)
+                real_inputs, target_inputs, _ = accelerator.unwrap_model(vlm_model).get_inputs(real_images, target_images, text_descriptions, device)
                 target_inputs["pixel_values"] = vp_model(target_inputs["pixel_values"])
                 loss = vlm_model(real_inputs, target_inputs)
 
