c53fa28f30cf4cdda794e8fa35e4cc252c25250f
SeungWon Seo <ttd8591@gmail.com>
2025-02-07 12:34:03 +0900

    using multiple models with deepspeed

--- vp_model/pre_utils.py
+++ vp_model/pre_utils.py
@@ -7,8 +7,8 @@ import numpy as np
 def parse_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument("--dataset_dir", type=str, default="C:\\Users\\ttd85\\Documents\\Github\LLM4Layout\\vp_model\\indoor_preprocessing\\outputs\\indoor_layouts")
-    # parser.add_argument("--dataset_dir", type=str, default="/local_datasets/llm4layout/outputs/indoor_layouts/")
-    parser.add_argument("--dataset_dir", type=str, default="./indoor_preprocessing/outputs/indoor_layouts/")
+    parser.add_argument("--dataset_dir", type=str, default="/local_datasets/llm4layout/outputs/indoor_layouts/")
+    # parser.add_argument("--dataset_dir", type=str, default="./indoor_preprocessing/outputs/indoor_layouts/")
     parser.add_argument("--save_dir", type=str, default="./vp_model_saves")
     parser.add_argument("--model_name", type=str, default="meta-llama/Llama-3.2-11B-Vision-Instruct")
     # parser.add_argument("--model_name", type=str, default="unsloth/Llama-3.2-11B-Vision-Instruct-bnb-4bit")
