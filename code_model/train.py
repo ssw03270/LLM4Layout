425bad269e567839a743224d3ae07c9596cf6f53
ssw03270 <ttd8591@gmail.com>
2025-02-14 17:18:34 +0900

    finetuning

--- code_model/train.py
+++ code_model/train.py
@@ -48,8 +48,8 @@ from llama_cookbook.utils.dataset_utils import get_dataloader
 train_dataset = load_dataset("json", data_files="../dataset/train_messages.jsonl")
 val_dataset = load_dataset("json", data_files="../dataset/val_messages.jsonl")
 
-train_dataset.dataset = "custom"
-val_dataset.dataset = "custom"
+train_dataset.dataset = "custom_dataset"
+val_dataset.dataset = "custom_dataset"
 
 train_dataloader = get_dataloader(tokenizer, train_dataset, train_config)
 eval_dataloader = get_dataloader(tokenizer, val_dataset, train_config, "val")
